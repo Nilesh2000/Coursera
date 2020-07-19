3a2fb9c083a3c4344e2773e64d84ac75a748d89a
Nilesh D <nileshlund@gmail.com>
2020-07-19 15:17:29 +0530

    Week 5 Assignment

---
+++ Building-Web-Applications-In-PHP/Week5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nilesh D</title>
+</head>
+<body>
+  <h1>Nilesh D</h1> 
+
+  <pre>
+    ASCII ART:
+      *           *
+      * *         *
+      *   *       *
+      *     *     *
+      *       *   *
+      *         * *
+      *           *
+  </pre>
+  <p>
+    The SHA256 Hash of "Nilesh D" is <?php echo hash('sha256', 'Nilesh D'); ?>
+  </p>
+</body>
+</html>
